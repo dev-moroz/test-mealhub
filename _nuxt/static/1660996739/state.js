8da5afb332cd2201b7ac4d03e1b514bb7caa6290
dev-moroz <dev_moroz@mail.ru>
2022-08-20 15:00:08 +0300

    a9ecb93

---
+++ _nuxt/static/1660996739/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-mealhub\u002F_nuxt\u002Fstatic\u002F1660996739",layout:"default",error:a,state:{counter:3,comments:[],sortComments:"asc"},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-mealhub\u002F",assetsPath:"\u002Ftest-mealhub\u002F_nuxt\u002F",cdnURL:a}}}}(null));
